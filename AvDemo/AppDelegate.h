3f3e8f4438b893252c0c8a04966d31866a53f591
Avadhesh Sharma <avadhesh.sharma@mobileprogrammingllc.com>
2016-05-31 22:28:52 +0530

    Push code

---
+++ AvDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AvDemo
+//
+//  Created by Avadhesh Sharma on 31/05/16.
+//  Copyright © 2016 Avadhesh Sharma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AvDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AvDemo
+//
+//  Created by Avadhesh Sharma on 31/05/16.
+//  Copyright © 2016 Avadhesh Sharma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
